6c1df2a168d143b3fdf5a3cf81b1a0b450101e6f
ElainaHalls <130600410+ElainaHalls@users.noreply.github.com>
2023-06-13 16:14:42 -0600

    Added a class to the models.py file

--- webview/models.py
+++ webview/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_missing = models.DateField(max_length=10)
+    age_at_missing = models.IntegerField(max_length=3)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=2)
+    gender = models.CharField(max_length=1)
+    race = models.CharField(max_length=1)
