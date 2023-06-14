5def5216497242490c5a80d97224cf6ac6a4655e
loaf <130481472+itloaf@users.noreply.github.com>
2023-06-14 12:09:05 -0600

    Update settings.py

--- missingppl/settings.py
+++ missingppl/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'missing_persons',
         'USER': 'postgres',
-        'PASSWORD': 'Nosaj.31.2001',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
