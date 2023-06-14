f97363ac2e8dbedccd74e5efb46d041fe153ee24
loaf <130481472+itloaf@users.noreply.github.com>
2023-06-14 09:30:25 -0600

    Update models.py

--- webview/models.py
+++ webview/models.py
@@ -5,7 +5,7 @@ class Person(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
   date_missing = models.DateField()
-  age_at_missing = models.IntegerField(max_length=3)
+  age_at_missing = models.IntegerField()
   city = models.CharField(max_length=20)
   state = models.CharField(max_length=2)
   gender = models.CharField(max_length=1)
