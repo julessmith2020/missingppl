b98361d2a6a9e1380b3c0256fcbeb5a94699488d
maddiehutch1 <134972856+maddiehutch1@users.noreply.github.com>
2023-06-14 20:02:26 -0600

    Update admin.py

--- webview/admin.py
+++ webview/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import Person
 
 # Register your models here.
+#helps the admin site connect with the Person database table in order to add values and inputs from the creator's end
+
 admin.site.register(Person)
