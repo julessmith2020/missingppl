d7ba825aae16452d19cabe15dac6927567aca7a0
maddiehutch1 <134972856+maddiehutch1@users.noreply.github.com>
2023-06-14 20:03:32 -0600

    Update urls.py

--- webview/urls.py
+++ webview/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# we created this webview urls.py in order to have access to the views of the server 
+# based on the way a user puts the url online (what goes after localhost:8000/)
 urlpatterns = [
     path("", views.indexPageView, name="index"),
     path("index/", views.indexPageView, name="index"),
